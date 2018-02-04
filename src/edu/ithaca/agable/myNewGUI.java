f974a65c88556267bb6f147098b83d68a1b60f26
Alex Gable <agable@ithaca.edu>
2018-02-04 17:33:27 -0500

    fUCK

--- src/edu/ithaca/agable/myNewGUI.java
+++ src/edu/ithaca/agable/myNewGUI.java
@@ -3,6 +3,8 @@ package edu.ithaca.agable;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class myNewGUI extends JFrame {
 
@@ -16,6 +18,35 @@ public class myNewGUI extends JFrame {
         regular = new JButton("Regular");
         add(regular);
 
+        ImageIcon redButton = createImageIcon("edu/ithaca/agable/redButton.png","my red button");
+        ImageIcon blueButton = createImageIcon("edu/ithaca/agable/blueButton.png","my blue button");
+//        Icon blueButton = new ImageIcon(getClass().getResource("C:\\Users\\Alex\\Documents\\2017-2018\\SP2018\\325\\Impl_01\\src\\edu\\ithaca\\agable\\blueButton.png"));
+//        Icon redButton = new ImageIcon(getClass().getResource("C:\\Users\\Alex\\Documents\\2017-2018\\SP2018\\325\\Impl_01\\src\\edu\\ithaca\\agable\\redButton.png"));
+        custom = new JButton("Custom",blueButton);
+        custom.setRolloverIcon(redButton);
+        add(custom);
 
+        HandlerClass handler = new HandlerClass();
+        regular.addActionListener(handler);
+        custom.addActionListener(handler);
+
+    }
+
+    protected ImageIcon createImageIcon(String path, String description){
+        java.net.URL imgURL = getClass().getResource(path);
+        if(imgURL!=null){
+            return new ImageIcon(imgURL,description);
+        }else{
+            System.err.println("Couldn't find file: " +path);
+            return null;
+        }
+    }
+
+    private class HandlerClass implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(null,String.format("%s",e.getActionCommand()));
+        }
     }
 }
---
+++ src/edu/ithaca/agable/myNewGUIMain.java
@@ -0,0 +1,12 @@
+package edu.ithaca.agable;
+
+import javax.swing.*;
+
+public class myNewGUIMain {
+    public static void main(String[] args) {
+        myNewGUI go = new myNewGUI();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300,200);
+        go.setVisible(true);
+    }
+}
